b4c0daf291008b91898e059fdff43b3b3b94034f
Twin <hi@twin.uk.com>
2016-11-15 14:04:56 +0000

    Removed cuDNN libraries and updated install script

--- iceberg/software/install_scripts/libs/binlibs/cudnn/cudnn_install.sh
+++ iceberg/software/install_scripts/libs/binlibs/cudnn/cudnn_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #The script installs cuDNN 5.0 for CUDA 7.5 and CUDA 8.0 in to the correct iceberg directory
+#The binaries can be downloaded from Nvidia at https://developer.nvidia.com/cudnn
 #We assume that the .tgz fils are in current working directory
 
 #Install cuDNN 5.0 for CUDA 7.5
